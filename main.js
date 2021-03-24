6a018f6d9464a73809399c58d11b7f280c3d33cc
JavaScript <6206021612156@fitm.kmutnb.ac.th>
2021-03-24 11:23:19 +0700

    commit

--- main.js
+++ main.js
@@ -6,9 +6,15 @@ const app = Vue.createApp({
             image: './assets/images/socks_blue.jpg',
             inStock: true,
             details: ['50% cotton', '30% wool', '20% polyester'],
+            sizes: ['S', 'M', 'L', 'XL'],
             variants: [
               { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
               { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
+              { id: 2236, color: 'Tanjiro', image: './assets/images/Tanjiro.jpg' },
+              { id: 2237, color: 'Nesigo', image: './assets/images/Nesigo.png' },
+              { id: 2238, color: 'giu', image: './assets/images/giu.png' },
+              { id: 2239, color: 'shinobu', image: './assets/images/shinobu.png' },
+              { id: 2240, color: 'Obanai', image: './assets/images/Obanai.png' },
             ]
         }
     },
